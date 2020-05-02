2da313b027f7d0368b314f665329e83c9fa216a5
Kastet <frontendkastet@gmail.com>
2020-05-03 00:32:04 +0300

    vse chto imeju na dannij moment

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.getElementById('burger').onclick = function(){
+  addMenu();
+};
+
+function addMenu(){
+  document.getElementById('menu').classList.toggle('show');
+};
+
+// ADD Slider
+
+setInterval(function(){
+if(document.querySelectorAll('.owl-dot')[0]){
+  document.getElementById("container1").classList.add('bgc1');
+} else if(document.querySelectorAll)
+},10);
