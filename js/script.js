1a635cb62c671799f03ad3e3053d24e92eed4112
Kastet <frontendkastet@gmail.com>
2020-05-16 21:15:19 +0300

    Vse chto imeju po mogo

--- js/script.js
+++ js/script.js
@@ -9,7 +9,11 @@ function addMenu(){
 // ADD Slider
 
 setInterval(function(){
-if(document.querySelectorAll('.owl-dot')[0]){
-  document.getElementById("container1").classList.add('bgc1');
-} else if(document.querySelectorAll)
-},10);
+  if(document.querySelectorAll('.owl-dot')[0].className === "owl-dot active"){
+    document.getElementById("container1").classList.remove('bgc2')
+    document.getElementById("container1").classList.add('bgc1')
+  } else if(document.querySelectorAll('.owl-dot')[1].className === "owl-dot active"){
+    document.getElementById("container1").classList.remove('bgc1')
+    document.getElementById("container1").classList.add('bgc2')
+  }
+  },10);
